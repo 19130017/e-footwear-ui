a2df427540bb863c8b1433135ff917fdff5d8bd7
Quang Huy <85470086+QuangHuyXX@users.noreply.github.com>
2023-03-12 14:06:05 +0700

    change Carousel

--- src/components/carousel/MyCarousel.js
+++ src/components/carousel/MyCarousel.js
@@ -5,6 +5,7 @@ import Slider from "react-slick";
 import style from "./MyCarousel.module.scss";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { dataCarouselExample } from "../../assets/dataCarousel/dataCarouselExample";
 
 const cx = classNames.bind(style);
 
@@ -16,40 +17,16 @@ function MyCarousel({}) {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
+    arrows: false,
   };
   return (
     <Box className={cx("main")}>
       <Slider {...settings}>
-        <Box className={cx("card")}>
-          <img
-            className={cx("picture")}
-            src="https://media.coolmate.me/cdn-cgi/image/width=1800,quality=90,format=auto/uploads/February2023/sBanner-Running2.png"
-          ></img>
-        </Box>
-        <Box className={cx("card")}>
-          <img
-            className={cx("picture")}
-            src="https://media.coolmate.me/cdn-cgi/image/width=1800,quality=90,format=auto/uploads/February2023/Banner-Combo.png"
-          ></img>
-        </Box>
-        <Box className={cx("card")}>
-          <img
-            className={cx("picture")}
-            src="https://media.coolmate.me/cdn-cgi/image/width=1800,quality=90,format=auto/uploads/February2023/sBanner-Running2.png"
-          ></img>
-        </Box>
-        <Box className={cx("card")}>
-          <img
-            className={cx("picture")}
-            src="https://media.coolmate.me/cdn-cgi/image/width=1800,quality=90,format=auto/uploads/February2023/Banner-Combo.png"
-          ></img>
-        </Box>
-        <Box className={cx("card")}>
-          <img
-            className={cx("picture")}
-            src="https://media.coolmate.me/cdn-cgi/image/width=1800,quality=90,format=auto/uploads/February2023/sBanner-Running2.png"
-          ></img>
-        </Box>
+        {dataCarouselExample.map((item) => (
+          <Box className={cx("card")}>
+            <img className={cx("picture")} src={item.linkImage}></img>
+          </Box>
+        ))}
       </Slider>
     </Box>
   );
