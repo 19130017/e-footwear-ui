1eb9cf278d5ea73b2089af61eb936b758dc30556
19130017 <19130017@st.hcmuaf.edu.vn>
2023-03-30 10:41:00 +0700

    update dropdown

---
+++ src/components/dropdown/DropdownAccount.js
@@ -0,0 +1,77 @@
+import { Box, Divider, List, ListItem, ListItemText } from "@mui/material";
+import classnames from "classnames/bind";
+import { Link } from "react-router-dom";
+import style from "./Dropdown.module.scss";
+import LoginIcon from "@mui/icons-material/Login";
+import LogoutIcon from "@mui/icons-material/Logout";
+import HelpIcon from "@mui/icons-material/Help";
+import HowToRegIcon from "@mui/icons-material/HowToReg";
+import ProfileIcon from "@mui/icons-material/AdminPanelSettings";
+const cx = classnames.bind(style);
+function DropdownAccount() {
+    const isLogin = false;
+    return (
+        <List className={cx("list")}>
+            {isLogin ? (
+                <>
+                    <Link to={"/account/profile"} className={cx("item-link")}>
+                        <ListItem>
+                            <ListItemText className={cx("item-text")}>
+                                <ProfileIcon className={cx("icon")} /> Thông tin cá nhân
+                            </ListItemText>
+                        </ListItem>
+                        <Divider />
+                    </Link>
+                    <Link to={"/auth/forgot"} className={cx("item-link")}>
+                        <ListItem>
+                            <ListItemText className={cx("item-text")}>
+                                <HelpIcon className={cx("icon")} />
+                                Quên mật khẩu
+                            </ListItemText>
+                        </ListItem>
+                    </Link>
+                    <Link to={"/auth/logout"} className={cx("item-link")}>
+                        <ListItem>
+                            <ListItemText className={cx("item-text")}>
+                                <LogoutIcon className={cx("icon")} />
+                                Đăng Xuất
+                            </ListItemText>
+                        </ListItem>
+                        <Divider />
+                    </Link>
+                </>
+            ) : (
+                <>
+                    <Link to={"/auth/sign-in"} className={cx("item-link")}>
+                        <ListItem>
+                            <ListItemText className={cx("item-text")}>
+                                <LoginIcon className={cx("icon")} />
+                                Đăng nhập
+                            </ListItemText>
+                        </ListItem>
+                        <Divider />
+                    </Link>
+                    <Link to={"/auth/sign-up"} className={cx("item-link")}>
+                        <ListItem>
+                            <ListItemText className={cx("item-text")}>
+                                <HowToRegIcon className={cx("icon")} />
+                                Đăng ký
+                            </ListItemText>
+                        </ListItem>
+                        <Divider />
+                    </Link>
+                    <Link to={"/auth/forgot"} className={cx("item-link")}>
+                        <ListItem>
+                            <ListItemText className={cx("item-text")}>
+                                <HelpIcon className={cx("icon")} />
+                                Quên mật khẩu
+                            </ListItemText>
+                        </ListItem>
+                    </Link>
+                </>
+            )}
+        </List>
+    );
+}
+
+export default DropdownAccount;
