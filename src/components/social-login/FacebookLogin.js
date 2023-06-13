167e7958e07af90806938cb63ee77d670c81b71b
Quang Huy <85470086+QuangHuyXX@users.noreply.github.com>
2023-06-13 20:11:20 +0700

    Update FacebookLogin.js

--- src/components/social-login/FacebookLogin.js
+++ src/components/social-login/FacebookLogin.js
@@ -1,5 +1,5 @@
 import { useCallback, useState } from "react";
-import { LoginSocialGoogle } from "reactjs-social-login";
+import { LoginSocialFacebook, LoginSocialGoogle } from "reactjs-social-login";
 import { FacebookLoginButton } from "react-social-login-buttons";
 
 import classNames from "classnames/bind";
@@ -8,37 +8,24 @@ import { useNavigate } from "react-router-dom";
 const cx = classNames.bind(style);
 
 function FacebookLogin() {
-    const [provider, setProvider] = useState("");
-    const [profile, setProfile] = useState();
     const onLoginStart = useCallback(() => {
-        alert("login start");
     }, []);
-
-    const onLogoutSuccess = useCallback(() => {
-        setProfile(null);
-        setProvider("");
-        alert("logout success");
-    }, []);
-
     return (
-        <LoginSocialGoogle
-            client_id="882148200553-rro95qim4oaucfj4nvmb66lkhpe1pmma.apps.googleusercontent.com"
-            // onLoginStart={onLoginStart}
-            redirect_uri={"http://localhost:3000"}
-            scope="openid profile email"
-            discoveryDocs="claims_supported"
-            onResolve={({ provider, data }) => {
-                setProvider(provider);
-                setProfile(data);
+        <LoginSocialFacebook
+            appId="298400012522825"
+            onLoginStart={onLoginStart}
+            // redirect_uri={process.env.REACT_APP_HOST}
+            onResolve={(response) => {
+                console.log(response);
             }}
-            onReject={(err) => {
-                console.log(err);
+            onReject={(error) => {
+                console.log(error);
             }}
         >
             <FacebookLoginButton className={cx("facebook-btn")} activeStyle={false}>
                 <span>Đăng nhập với Facebook</span>
             </FacebookLoginButton>
-        </LoginSocialGoogle>
+        </LoginSocialFacebook>
     );
 }
 
