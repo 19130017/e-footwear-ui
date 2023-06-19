b3bf70333e6e655da6c98bd2d9e288bb784b7f0e
19130017 <19130017@st.hcmuaf.edu.vn>
2023-06-19 16:11:52 +0700

    fix chatbot #5

--- src/components/chatbot/ChatBot.js
+++ src/components/chatbot/ChatBot.js
@@ -1,30 +1,33 @@
 import { Box } from "@mui/material";
+import { useEffect, useRef } from "react";
 
 function ChatBot() {
-    window.fbAsyncInit = function () {
-        window.FB.init({
-            xfbml: true,
-            version: "v17.0",
-        });
-    };
+    const MessengerRef = useRef();
+    useEffect(() => {
+        window.fbAsyncInit = function () {
+            window.FB.init({
+                xfbml: true,
+                version: "v17.0",
+            });
+        };
 
-    (function (d, s, id) {
-        var js,
-            fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    })(document, "script", "facebook-jssdk");
+        (function (d, s, id) {
+            var js,
+                fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        })(document, "script", "facebook-jssdk");
 
-    var chatbot = document.getElementById("fb-customer-chat");
-    chatbot?.setAttribute("page_id", "103824452759818");
-    chatbot?.setAttribute("attribution", "biz_inbox");
+        MessengerRef.current.setAttribute("page_id", "103824452759818");
+        MessengerRef.current.setAttribute("attribution", "biz_inbox");
+    }, []);
     return (
         <Box>
             <div id="fb-root"></div>
-            <div id="fb-customer-chat" className="fb-customerchat"></div>
+            <div ref={MessengerRef} id="fb-customer-chat" className="fb-customerchat"></div>
         </Box>
     );
 }
