dbf6729b67eb69a394006be22adc930341d64534
catabada <huynhhuuan10121972@gmail.com>
2023-06-25 14:51:11 +0700

    Voice Text

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,23 +1,76 @@
 import { Box, IconButton, InputBase } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
+import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice';
+import SettingsVoiceIcon from '@mui/icons-material/SettingsVoice';
+import useSpeechToText from 'react-hook-speech-to-text';
 import style from "./Search.module.scss";
 import classnames from "classnames/bind";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const cx = classnames.bind(style);
-function Search() {
+const Search = () => {
     const navigate = useNavigate();
     const [query, setQuery] = useState("");
+    var timer;
+    const {
+        error,
+        interimResult,
+        isRecording,
+        results,
+        startSpeechToText,
+        stopSpeechToText,
+    } = useSpeechToText({
+        speechRecognitionProperties: {
+            lang: 'vi-VN',
+            interimResults: true // Allows for displaying real-time speech results
+        },
+        continuous: true,
+        googleApiKey: 'AIzaSyB0Gsdi8t8o0ljBfBKAIGTqClcnfLyj3Dk',
+        useLegacyResults: false,
+        crossBrowser: true,
+    });
+
+
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         navigate({ pathname: "/search", search: `?query=${query}` });
         setQuery("");
     };
+
+    const handleRequestSearchByVoice = async () => {
+        navigate({ pathname: "/search", search: `?query=${query}` });
+        setQuery("");
+    }
+
     const handleChange = (e) => {
         const value = e.target.value;
         setQuery(value);
     };
+
+    useEffect(() => {
+        if (interimResult && isRecording) {
+            setQuery(interimResult);
+        }
+        if (typeof timer !== "undefined") {
+            clearTimeout(timer);
+        }
+
+        timer = setTimeout(() => {
+            if (interimResult) {
+                stopSpeechToText();
+                // resetTranscript();
+                handleRequestSearchByVoice();
+            }
+
+        }, 2000);
+
+
+        return () => clearTimeout(timer);
+
+    }, [query, interimResult, isRecording]);
+
     return (
         <Box
             component={"form"}
@@ -30,6 +83,25 @@ function Search() {
             className={cx("icon-wrapper")}
             onSubmit={(e) => handleSubmit(e)}
         >
+            {
+                <IconButton
+                    aria-label="search"
+                    className={cx("icon-btn")}
+                    sx={{ width: "20%", padding: "0 10px" }}
+                    onClick={!isRecording ? startSpeechToText : stopSpeechToText}
+                >
+                    {!isRecording ? <KeyboardVoiceIcon
+                        color="error"
+                        sx={{ padding: "0 2px" }}
+                        className={cx("icon")} />
+                        : <SettingsVoiceIcon
+                            color="error"
+                            sx={{ padding: "0 2px" }}
+                            className={cx("icon")} />
+                    }
+
+                </IconButton>
+            }
             <InputBase
                 sx={{
                     ml: 1,
